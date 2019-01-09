460b8420bef6c53aa7eb6f45ea0cf7b7f6701f8e
dragos-vlasie <dragos.vlasie@thisplace.com>
2019-01-09 09:35:34 +0000

    Added some hmtl to the react components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter} from 'react-router-dom'
+import { BrowserRouter, Switch, Route} from 'react-router-dom'
+
+import Navbar from './components/layout/Navbar'
+import Dashboard from './components/dashboard/Dashboard';
 
 
 class App extends Component {
@@ -7,7 +10,10 @@ class App extends Component {
     return (
       <BrowserRouter>
         <div className="App">
-            <h1>Project Planer</h1>
+          <Navbar />
+          <Switch>
+            <Route path='/' component={Dashboard}/>
+          </Switch>
         </div>
       </BrowserRouter>
     );
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+    return (
+        <div className="project-list section">
+            <div className="div card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <div className="span card-title">Project Title</div>
+                    <p>Posted by Dragos Vlasie</p>   
+                    <p className="grey-text">3rd September</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectSummary
