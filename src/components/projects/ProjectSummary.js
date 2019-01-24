076f3963914f82e2ed42050f1d4c2c03330ffb06
dragos-vlasie <dragos.vlasie@thisplace.com>
2019-01-24 17:06:02 +0000

    installed thunk also set up data comunication between the reducer and components

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
+    console.log('project:', project)
     return (
         <div className="project-list section">
             <div className="div card z-depth-0 project-summary">
                 <div className="card-content grey-text text-darken-3">
-                    <div className="span card-title">Project Title</div>
+                    <div className="span card-title">{project.title}</div>
                     <p>Posted by Dragos Vlasie</p>   
                     <p className="grey-text">3rd September</p>
                 </div>
