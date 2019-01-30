f1677de68e7f8a34465df226bdb6e602b507aaef
dragos-vlasie <dragos.vlasie@thisplace.com>
2019-01-30 10:00:42 +0000

    borken need fixing

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -2,6 +2,17 @@ export const createProject = (project) => {
     return (dispatch, getState, { getFirebase, getFirestore }) => {
         // make async call to database 
         // passs in the action
-        dispatch({type: 'CREATE_PROJECT', project: project });
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'Dragos',
+            authorLastName: 'Vlasie',
+            authorId: 12345,
+            createdAt: 20
+        }).then(() => {
+            dispatch({type: 'CREATE_PROJECT', project: project });
+        }).catch((err) => {
+            dispatch({ type: 'CREATE_PROJECT_ERROR', err })
+        })
     }
 };
