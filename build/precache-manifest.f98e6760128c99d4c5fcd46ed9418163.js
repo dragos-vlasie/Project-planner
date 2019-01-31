e05769f35a33666d5055b629208f21cf0c6a1408
dragos-vlasie <dragos.vlasie@thisplace.com>
2019-01-31 16:32:22 +0000

    build the project

---
+++ build/precache-manifest.f98e6760128c99d4c5fcd46ed9418163.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1db97f85391c3305eaa1",
+    "url": "/static/js/main.1db97f85.chunk.js"
+  },
+  {
+    "revision": "ad84e2a675ca5e7941b0",
+    "url": "/static/js/1.ad84e2a6.chunk.js"
+  },
+  {
+    "revision": "1db97f85391c3305eaa1",
+    "url": "/static/css/main.3f3141b7.chunk.css"
+  },
+  {
+    "revision": "2b19a6d1d4482ed14330f8f1928e96f2",
+    "url": "/index.html"
+  }
+];
